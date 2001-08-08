e9c405fc346423678248d19899a0fcb76ed011e3
Bruce Guenter <bruce@untroubled.org>
2001-08-08 05:18:36 +0000

    Adapted to new insthier framework.

--- insthier.c
+++ insthier.c
@@ -3,6 +3,6 @@
 
 void insthier(void) {
   int bin = opendir(conf_bin);
-  c(bin, 0, "smtpfront-qmail", -1, -1, 0755);
-  c(bin, 0, "smtpfront-test",  -1, -1, 0755);
+  c(bin, "smtpfront-qmail", -1, -1, 0755);
+  c(bin, "smtpfront-test",  -1, -1, 0755);
 }
