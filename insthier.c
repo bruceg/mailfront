5fda8b73a51923aaab468846bdae6518b0794d57
Bruce Guenter <bruce@untroubled.org>
2001-08-09 02:04:36 +0000

    *** empty log message ***

--- insthier.c
+++ insthier.c
@@ -3,6 +3,7 @@
 
 void insthier(void) {
   int bin = opendir(conf_bin);
-  c(bin, "smtpfront-qmail", -1, -1, 0755);
-  c(bin, "smtpfront-test",  -1, -1, 0755);
+  c(bin, "smtpfront-echo",   -1, -1, 0755);
+  c(bin, "smtpfront-qmail",  -1, -1, 0755);
+  c(bin, "smtpfront-reject", -1, -1, 0755);
 }
