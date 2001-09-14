06e8d1a973c04ff9723ccebd25ccd7384586034b
Bruce Guenter <bruce@untroubled.org>
2001-09-14 11:45:26 +0000

    Lowercase domain names before comparing them.

--- qmail-validate.c
+++ qmail-validate.c
@@ -11,15 +11,24 @@ static dict rh;
 static dict brt;
 static str tmp;
 
+static int lower(str* s)
+{
+  str_lower(s);
+  return 1;
+}
+
 const response* qmail_validate_init(void)
 {
   static const response resp_no_chdir = {0,451,"Could not change to the qmail directory."};
   static const response resp_error = {0,451,"Internal error."};
   
   if (chdir(conf_qmail) == -1) return &resp_no_chdir;
-  if (!dict_load_list(&bmf, "control/badmailfrom", 0)) return &resp_error;
-  if (!dict_load_list(&rh, "control/rcpthosts", 0)) return &resp_error;
-  if (!dict_load_list(&brt, "control/badrcptto", 0)) return &resp_error;
+  if (!dict_load_list(&bmf, "control/badmailfrom", 0, lower))
+    return &resp_error;
+  if (!dict_load_list(&rh, "control/rcpthosts", 0, lower))
+    return &resp_error;
+  if (!dict_load_list(&brt, "control/badrcptto", 0, lower))
+    return &resp_error;
   return 0;
 }
 
@@ -31,6 +40,7 @@ const response* qmail_validate_sender(const str* sender)
   if ((at = str_findlast(sender, '@')) > 0) {
     ++at;
     str_copyb(&tmp, sender->s + at, sender->len - at);
+    str_lower(&tmp);
     if (dict_get(&bmf, &tmp)) return &resp;
   }
   return 0;
@@ -56,11 +66,15 @@ const response* qmail_validate_recipient(const str* recipient)
   static const response resp_bmt = {0,553,"Sorry, that address is in my badrcptto list."};
   int at;
 
-  if (dict_get(&brt, recipient) != 0) return &resp_bmt;
+  str_copy(&tmp, recipient);
+  str_lower(&tmp);
+  if (dict_get(&brt, &tmp) != 0) return &resp_bmt;
   if ((at = str_findlast(recipient, '@')) > 0) {
     ++at;
     str_copyb(&tmp, recipient->s + at, recipient->len - at);
+    str_lower(&tmp);
     if (dict_get(&rh, &tmp)) return 0;
+    /* NOTE: qmail-newmrh automatically lowercases the keys in this CDB */
     if (cdb_lookup("control/morercpthosts.cdb", &tmp)) return 0;
     return &resp_rh;
   }
