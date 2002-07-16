90239db7588478c5bb34f98ea58605e8d48fa375
Bruce Guenter <bruce@untroubled.org>
2002-07-16 05:58:02 +0000

    Added missing imapfront-auth.

--- insthier.c
+++ insthier.c
@@ -3,9 +3,10 @@
 
 void insthier(void) {
   int bin = opendir(conf_bin);
+  c(bin, "imapfront-auth",    -1, -1, 0755);
+  c(bin, "pop3front-auth",    -1, -1, 0755);
+  c(bin, "pop3front-maildir", -1, -1, 0755);
   c(bin, "smtpfront-echo",    -1, -1, 0755);
   c(bin, "smtpfront-qmail",   -1, -1, 0755);
   c(bin, "smtpfront-reject",  -1, -1, 0755);
-  c(bin, "pop3front-auth",    -1, -1, 0755);
-  c(bin, "pop3front-maildir", -1, -1, 0755);
 }
