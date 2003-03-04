09b06bbb510bca384370c7d4cd643f4de1250906
Bruce Guenter <bruce@untroubled.org>
2003-03-04 06:55:23 +0000

    Install the new QMQP and QMTP programs.

--- insthier.c
+++ insthier.c
@@ -6,6 +6,8 @@ void insthier(void) {
   c(bin, "imapfront-auth",    -1, -1, 0755);
   c(bin, "pop3front-auth",    -1, -1, 0755);
   c(bin, "pop3front-maildir", -1, -1, 0755);
+  c(bin, "qmqpfront-qmail",   -1, -1, 0755);
+  c(bin, "qmtpfront-qmail",   -1, -1, 0755);
   c(bin, "smtpfront-echo",    -1, -1, 0755);
   c(bin, "smtpfront-qmail",   -1, -1, 0755);
   c(bin, "smtpfront-reject",  -1, -1, 0755);
