961f260d5dbd5317919c9a41d90df16664b7b25f
Bruce Guenter <bruce@untroubled.org>
2004-12-03 18:26:40 +0000

    Fixed bug in pattern matching where the pattern was longer than the line that would other match.  This relied on the erroneous assumption that patterns would always be shorter than the lines they were intended to match.

--- patterns.c
+++ patterns.c
@@ -100,8 +100,7 @@ static const response* check_line()
   const str fakeline = { linebuf, linepos, 0 };
   linebuf[linepos] = 0;
   for (p = patterns, i = 0; i < pattern_count; ++i, ++p) {
-    if (linepos >= p->s.len &&
-	(!p->after_blank || after_blank) &&
+    if ((!p->after_blank || after_blank) &&
 	str_glob(&fakeline, &p->s)) {
       resp_patmatch.message = p->message;
       return &resp_patmatch;
