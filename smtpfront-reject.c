21dad8c936b21068983bcdaba3e1c7099ef76288
Bruce Guenter <bruce@untroubled.org>
2005-10-20 20:15:47 +0000

    Eliminate unused variable warnings in smtpfront-reject.c

--- smtpfront-reject.c
+++ smtpfront-reject.c
@@ -22,21 +22,25 @@ void backend_handle_reset(void)
 const response* backend_validate_sender(str* unused)
 {
   return &resp;
+  (void)unused;
 }
 
 const response* backend_validate_recipient(str* unused)
 {
   return &resp;
+  (void)unused;
 }
 
 const response* backend_handle_sender(str* unused)
 {
   return &resp;
+  (void)unused;
 }
 
 const response* backend_handle_recipient(str* unused)
 {
   return &resp;
+  (void)unused;
 }
 
 const response* backend_handle_data_start(void)
@@ -46,6 +50,8 @@ const response* backend_handle_data_start(void)
 
 void backend_handle_data_bytes(const char* unused1, unsigned long unused2)
 {
+  (void)unused1;
+  (void)unused2;
 }
 
 const response* backend_handle_data_end(void)
@@ -68,4 +74,5 @@ int main(int argc, char* argv[])
     execvp(argv[1], argv+1);
     return 1;
   }
+  (void)argc;
 }
