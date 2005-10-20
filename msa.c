c8f20de6360d57b9b321f130b216228a43cd6944
Bruce Guenter <bruce@untroubled.org>
2005-10-20 04:56:28 +0000

    Added support for adding a default host and domain to addresses that are missing them in MSA mode.

--- msa.c
+++ msa.c
@@ -1,21 +1,41 @@
+#include <stdlib.h>
+#include <msg/wrap.h>
 #include "mailfront.h"
 
 static RESPONSE(nodomain,554,"Address is missing a domain name");
 static RESPONSE(nofqdn,554,"Address does not contain a fully qualified domain name");
 
+const char *defaultdomain;
+const char *defaulthost;
+
 static const response* check_fqdn(str* s)
 {
   int at;
   int dot;
-  if ((at = str_findlast(s, '@')) <= 0)
-    return &resp_nodomain;
-  if ((dot = str_findlast(s, '.')) < at)
-    return &resp_nofqdn;
+  if ((at = str_findlast(s, '@')) <= 0) {
+    if (defaulthost == 0)
+      return &resp_nodomain;
+    else {
+      at = s->len;
+      wrap_str(str_catc(s, '@'));
+      wrap_str(str_cats(s, defaulthost));
+    }
+  }
+  if ((dot = str_findlast(s, '.')) < at) {
+    if (defaultdomain == 0)
+      return &resp_nofqdn;
+    else {
+      wrap_str(str_catc(s, '.'));
+      wrap_str(str_cats(s, defaultdomain));
+    }
+  }
   return 0;
 }
 
 const response* msa_validate_init(void)
 {
+  defaultdomain = getenv("DEFAULTDOMAIN");
+  defaulthost = getenv("DEFAULTHOST");
   return 0;
 }
 
