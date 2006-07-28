746a98a8d6faf3e59fa3fcbf6ae98cd73795f671
Bruce Guenter <bruce@untroubled.org>
2006-07-28 15:46:59 +0000

    Since the require-auth plugin is optional, there is no need for it to check for $REQUIRE_AUTH.

--- plugin-require-auth.c
+++ plugin-require-auth.c
@@ -4,8 +4,7 @@ static RESPONSE(mustauth, 530, "5.7.1 You must authenticate first.");
 
 static const response* sender(str* s)
 {
-  if (session_getenv("REQUIRE_AUTH") != 0
-      && session.authenticated == 0
+  if (session.authenticated == 0
       && session.relayclient == 0)
     return &resp_mustauth;
   return 0;
