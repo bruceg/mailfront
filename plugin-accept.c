6b2dda804cf67093dee966a5a35048ca12b662ab
Bruce Guenter <bruce@untroubled.org>
2006-07-30 04:23:11 +0000

    Added a trivial 'accept' plugin, which accepts all senders and recipients.

---
+++ plugin-accept.c
@@ -0,0 +1,14 @@
+#include "mailfront.h"
+
+static RESPONSE(accept,250,0);
+
+static const response* address(str* s)
+{
+  return &resp_accept;
+  (void)s;
+}
+
+STRUCT_PLUGIN(accept) = {
+  .sender = address,
+  .recipient = address,
+};
