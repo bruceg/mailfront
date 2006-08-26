3a73f2b144c930d5a879c31a68b541bb5d3d9609
Bruce Guenter <bruce@untroubled.org>
2006-08-26 00:04:45 +0000

    Modified the *front-qmail wrapper scripts to included the expected list of plugins, and modified the documentation to make note of this fact.

--- smtpfront-qmail.sh
+++ smtpfront-qmail.sh
@@ -1,1 +1,1 @@
-exec "$(dirname $0)"/mailfront smtp qmail "$@"
+exec "$(dirname $0)"/mailfront smtp qmail check-fqdn counters mailrules relayclient qmail-validate cvm-validate add-received patterns accept-sender "$@"
