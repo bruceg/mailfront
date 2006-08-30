25e9c05fb763f38fdbb0a33ddaad2e7efd7f0b38
Bruce Guenter <bruce@untroubled.org>
2006-08-30 17:11:04 +0000

    Reversed the order of cvm-validate and qmail-validate in the wrapper scripts (and documentation) due to the semantics of the two plugins.

--- smtpfront-qmail.sh
+++ smtpfront-qmail.sh
@@ -1,1 +1,1 @@
-exec "$(dirname $0)"/mailfront smtp qmail check-fqdn counters mailrules relayclient qmail-validate cvm-validate add-received patterns accept-sender "$@"
+exec "$(dirname $0)"/mailfront smtp qmail check-fqdn counters mailrules relayclient cvm-validate qmail-validate add-received patterns accept-sender "$@"
