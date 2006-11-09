c9b3a157151246cc35930cffde2c417c7720731e
Bruce Guenter <bruce@untroubled.org>
2006-11-09 06:22:57 +0000

    Modified the echo backend to get its data from the temporary file and not use the data_block call.

--- backend-echo.c
+++ backend-echo.c
@@ -1,17 +1,12 @@
+#include <sysdeps.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <msg/msg.h>
 #include "mailfront.h"
 
 static response resp = { 250, 0 };
 static str tmp;
-
-static unsigned long databytes = 0;
-
-static const response* reset(void)
-{
-  databytes = 0;
-  return 0;
-}
 
 static const response* sender(str* s)
 {
@@ -31,26 +26,29 @@ static const response* recipient(str* r)
   return &resp;
 }
 
-static const response* data_block(const char* bytes, unsigned long len)
-{
-  databytes += len;
-  if (databytes == len) {
-    /* First line is always Received, log the first two lines. */
-    const char* ch;
-    ch = strchr(bytes, '\n');
-    str_copyb(&tmp, bytes, ch-bytes);
-    bytes = ch + 1;
-    ch = strchr(bytes, '\n');
-    str_catb(&tmp, bytes, ch-bytes);
-    msg1(tmp.s);
-  }
-  return 0;
-}
-
 static const response* message_end(int fd)
 {
+  struct stat st;
+  char buf[1024];
+  long rd;
+  char* lf;
+  char* ptr;
+
+  /* Log the first two lines of the message, usually a Received: header */
+  lseek(fd, 0, SEEK_SET);
+  rd = read(fd, buf, sizeof buf - 1);
+  buf[rd] = 0;
+  if ((lf = strchr(buf, LF)) != 0) {
+    str_copyb(&tmp, buf, lf-buf);
+    ptr = lf + 1;
+    if ((lf = strchr(ptr, LF)) != 0)
+      str_catb(&tmp, ptr, lf-ptr);
+    msg1(tmp.s);
+  }
+
+  fstat(fd, &st);
   str_copys(&tmp, "Received ");
-  str_catu(&tmp, databytes);
+  str_catu(&tmp, st.st_size);
   str_cats(&tmp, " bytes.");
   resp.message = tmp.s;
   return &resp;
@@ -58,9 +56,7 @@ static const response* message_end(int fd)
 }
 
 struct plugin backend = {
-  .reset = reset,
   .sender = sender,
   .recipient = recipient,
-  .data_block = data_block,
   .message_end = message_end,
 };
