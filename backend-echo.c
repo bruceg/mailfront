bd1afeee62b18e78ba24b8922779c2b435024032
Bruce Guenter <bruce@untroubled.org>
2006-11-12 20:39:16 +0000

    Adapted the backend to work both with and without a temporary file.

--- backend-echo.c
+++ backend-echo.c
@@ -7,6 +7,13 @@
 
 static response resp = { 250, 0 };
 static str tmp;
+static unsigned long databytes = 0;
+
+static const response* reset(void)
+{
+  databytes = 0;
+  return 0;
+}
 
 static const response* sender(str* s)
 {
@@ -26,6 +33,25 @@ static const response* recipient(str* r)
   return &resp;
 }
 
+static const response* data_block(const char* bytes, unsigned long len)
+{
+  if (session.fd < 0) {
+    str_copyb(&tmp, bytes, len);
+    if (databytes == 0) {
+      /* First line is always Received, log the first two lines. */
+      const char* ch;
+      ch = strchr(bytes, '\n');
+      str_copyb(&tmp, bytes, ch-bytes);
+      bytes = ch + 1;
+      if ((ch = strchr(bytes, '\n')) != 0)
+	str_catb(&tmp, bytes, ch-bytes);
+      msg1(tmp.s);
+    }
+    databytes += len;
+  }
+  return 0;
+}
+
 static const response* message_end(int fd)
 {
   struct stat st;
@@ -34,21 +60,24 @@ static const response* message_end(int fd)
   char* lf;
   char* ptr;
 
-  /* Log the first two lines of the message, usually a Received: header */
-  lseek(fd, 0, SEEK_SET);
-  rd = read(fd, buf, sizeof buf - 1);
-  buf[rd] = 0;
-  if ((lf = strchr(buf, LF)) != 0) {
-    str_copyb(&tmp, buf, lf-buf);
-    ptr = lf + 1;
-    if ((lf = strchr(ptr, LF)) != 0)
-      str_catb(&tmp, ptr, lf-ptr);
-    msg1(tmp.s);
-  }
+  if (fd >= 0) {
+    /* Log the first two lines of the message, usually a Received: header */
+    lseek(fd, 0, SEEK_SET);
+    rd = read(fd, buf, sizeof buf - 1);
+    buf[rd] = 0;
+    if ((lf = strchr(buf, LF)) != 0) {
+      str_copyb(&tmp, buf, lf-buf);
+      ptr = lf + 1;
+      if ((lf = strchr(ptr, LF)) != 0)
+	str_catb(&tmp, ptr, lf-ptr);
+      msg1(tmp.s);
+    }
 
-  fstat(fd, &st);
+    fstat(fd, &st);
+    databytes = st.st_size;
+  }
   str_copys(&tmp, "Received ");
-  str_catu(&tmp, st.st_size);
+  str_catu(&tmp, databytes);
   str_cats(&tmp, " bytes.");
   resp.message = tmp.s;
   return &resp;
@@ -56,7 +85,9 @@ static const response* message_end(int fd)
 }
 
 struct plugin backend = {
+  .reset = reset,
   .sender = sender,
   .recipient = recipient,
+  .data_block = data_block,
   .message_end = message_end,
 };
