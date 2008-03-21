550e3eb62c862442cf25b7b8e7e48e6bc5603389
Bruce Guenter <bruce@untroubled.org>
2008-03-21 08:54:47 -0600

    Fixed crash in plugin cvm-validate when the lookup secret was unset.

--- plugin-cvm-validate.c
+++ plugin-cvm-validate.c
@@ -6,22 +6,35 @@
 
 static const char* lookup_secret;
 static const char* cvm_lookup;
+static int cred_count;
 
 static RESPONSE(norcpt,553,"5.1.1 Sorry, that recipient does not exist.");
 static RESPONSE(failed,451,"4.1.0 Sorry, I could not verify that recipient (internal temporary error).");
 
 static const response* validate_init(void)
 {
-  cvm_lookup = getenv("CVM_LOOKUP");
-  if ((lookup_secret = getenv("CVM_LOOKUP_SECRET")) == 0)
-    lookup_secret = getenv("LOOKUP_SECRET");
-  /* Match the behavior of the current CVM code base: If
-   * $CVM_LOOKUP_SECRET is set to an empty string, treat it as if no
-   * lookup secret is required.  (If $CVM_LOOKUP_SECRET is unset, the
-   * module will not operate in lookup mode).
-   */
-  if (lookup_secret != 0 && *lookup_secret == 0)
-    lookup_secret = 0;
+  if ((cvm_lookup = getenv("CVM_LOOKUP")) != 0) {
+
+    if ((lookup_secret = getenv("CVM_LOOKUP_SECRET")) == 0)
+      lookup_secret = getenv("LOOKUP_SECRET");
+
+    /* Invoking CVMs in cvm-command mode without $CVM_LOOKUP_SECRET set
+     * will fail, since the CVM will be expecting additional credentials
+     * to validate.  To prevent this failure, set $CVM_LOOKUP_SECRET to
+     * an empty string. */
+    if (lookup_secret == 0) {
+      if (putenv("CVM_LOOKUP_SECRET=") != 0)
+	return &resp_oom;
+      /* Also set the lookup secret to an empty string internally to
+       * avoid NULL pointer issues later. */
+      lookup_secret = "";
+    }
+
+    /* Match the behavior of the current CVM code base: If
+     * $CVM_LOOKUP_SECRET is set to an empty string, treat it as if no
+     * lookup secret is required. */
+    cred_count = (*lookup_secret == 0) ? 2 : 3;
+  }
   return 0;
 }
 
@@ -41,7 +54,7 @@ static const response* validate_recipient(str* recipient)
   if (str_copyb(&creds[0].value, recipient->s, i)
       && str_copyb(&creds[1].value, recipient->s+i+1, recipient->len-i-1)
       && str_copys(&creds[2].value, lookup_secret)) {
-    switch (cvm_authenticate(cvm_lookup, 3, creds)) {
+    switch (cvm_authenticate(cvm_lookup, cred_count, creds)) {
     case 0: r = 0; break;
     case CVME_PERMFAIL: r = &resp_norcpt;
     }
