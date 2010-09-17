e8b1fd8e44927910c81a6bb7c9248a8af76dbd5e
Bruce Guenter <bruce@untroubled.org>
2010-09-16 21:25:49 -0600

    plugin-check-fqdn: Explicitly reject empty recipients, and add self-test

--- plugin-check-fqdn.c
+++ plugin-check-fqdn.c
@@ -2,6 +2,7 @@
 
 static RESPONSE(nodomain,554,"5.1.2 Address is missing a domain name");
 static RESPONSE(nofqdn,554,"5.1.2 Address does not contain a fully qualified domain name");
+static RESPONSE(notempty,554,"5.1.2 Recipient address may not be empty");
 
 static const response* either(str* s)
 {
@@ -32,8 +33,15 @@ static const response* either(str* s)
   return 0;
 }
 
+static const response* recipient(str* s)
+{
+  if (s->len == 0)
+    return &resp_notempty;
+  return either(s);
+}
+
 struct plugin plugin = {
   .version = PLUGIN_VERSION,
   .sender = either,
-  .recipient = either,
+  .recipient = recipient,
 };
